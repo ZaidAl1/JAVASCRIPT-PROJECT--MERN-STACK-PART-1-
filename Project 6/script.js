6a06970161d6007fc8c9f812bf223e8bf0520785
ZaidAl1 <68479914+ZaidAl1@users.noreply.github.com>
2021-09-07 17:02:33 +0500

    Peoject six completed

---
+++ Project 6/script.js
@@ -0,0 +1,17 @@
+const menuToggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+
+menuToggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav')
+});
+
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+window.addEventListener('click', e => 
+e.target===modal ? modal.classList.remove('show-modal') : false
+)
